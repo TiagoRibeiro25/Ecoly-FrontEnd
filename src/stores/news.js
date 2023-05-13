ae15ac3e3f5bf2825faacfc2cad68704ac9a9452
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-13 15:11:10 +0100

    Created news store

---
+++ src/stores/news.js
@@ -0,0 +1,17 @@
+import api from "@/services/api";
+import { defineStore } from "pinia";
+
+export const useNewsStore = defineStore("news", () => {
+	//TODO: fix the backend to return the correct data
+	const search = async (input) => {
+		try {
+			const response = await api.get(`/news?search=${input}`);
+			return response.data;
+		} catch (err) {
+			console.log(err.response.data || "Error in search news");
+			return { success: false, data: [] };
+		}
+	};
+
+	return { search };
+});

8346f4146f139134ab101453416517fc444fea3b
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-13 20:55:21 +0100

    Update ActivitiesView.vue

--- src/pages/ActivitiesView.vue
+++ src/pages/ActivitiesView.vue
@@ -1,5 +1,16 @@
+<script setup>
+import { useDark } from "@vueuse/core";
+import SearchBar from "../components/SearchBar.vue";
+import Header from "../components/Header.vue";
+
+const isDark = useDark();
+</script>
+
 <template>
-	<div>
-		<h1 class="text-center">Activities</h1>
+	<div class="col-12 d-flex align-items-center mt-5">
+		<SearchBar page="Activities" placeholder="Pesquise por atividades" />
+	</div>
+	<div class="col-12">
+		<Header title="ATIVIDADES" />
 	</div>
 </template>

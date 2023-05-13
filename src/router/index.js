e0c10f97a1655a8f86730e7894d880ed2c8bb52d
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-13 18:01:40 +0100

    Improved router js file

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,12 @@
+import ActivitiesView from "@/pages/ActivitiesView.vue";
+import AuthenticateView from "@/pages/AuthenticateView.vue";
+import DashboardView from "@/pages/DashboardView.vue";
 import HomeView from "@/pages/HomeView.vue";
+import ManageView from "@/pages/ManageView.vue";
+import NewsView from "@/pages/NewsView.vue";
+import NotFoundView from "@/pages/NotFoundView.vue";
+import ProfileView from "@/pages/ProfileView.vue";
+
 import { createRouter, createWebHistory } from "vue-router";
 import { useUsersStore } from "../stores/users";
 
@@ -14,37 +22,44 @@ const router = createRouter({
 		{
 			path: "/:pathMatch(.*)*",
 			name: "NotFound",
-			component: () => import("@/pages/NotFoundView.vue"),
+			// component: () => import("@/pages/NotFoundView.vue"),
+			component: NotFoundView,
 		},
 		{
 			path: "/news",
 			name: "News",
-			component: () => import("@/pages/NewsView.vue"),
+			// component: () => import("@/pages/NewsView.vue"),
+			component: NewsView,
 		},
 		{
 			path: "/activities",
 			name: "Activities",
-			component: () => import("@/pages/ActivitiesView.vue"),
+			// component: () => import("@/pages/ActivitiesView.vue"),
+			component: ActivitiesView,
 		},
 		{
 			path: "/dashboard",
 			name: "Dashboard",
-			component: () => import("@/pages/DashboardView.vue"),
+			// component: () => import("@/pages/DashboardView.vue"),
+			component: DashboardView,
 		},
 		{
 			path: "/manage",
 			name: "Manage",
-			component: () => import("@/pages/ManageView.vue"),
+			// component: () => import("@/pages/ManageView.vue"),
+			component: ManageView,
 		},
 		{
 			path: "/profile/:id",
 			name: "Profile",
-			component: () => import("@/pages/ProfileView.vue"),
+			// component: () => import("@/pages/ProfileView.vue"),
+			component: ProfileView,
 		},
 		{
 			path: "/authenticate",
 			name: "Authenticate",
-			component: () => import("@/pages/AuthenticateView.vue"),
+			// component: () => import("@/pages/AuthenticateView.vue"),
+			component: AuthenticateView,
 			beforeEnter: (to, from, next) => {
 				const usersStore = useUsersStore();
 				if (usersStore.isUserLoggedIn) next({ name: "Profile", params: { id: "me" } });

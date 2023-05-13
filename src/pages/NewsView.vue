e84f561fe8a1a7f4ac3aa28e1f5af764ad998371
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-13 20:55:23 +0100

    Update NewsView.vue

--- src/pages/NewsView.vue
+++ src/pages/NewsView.vue
@@ -1,5 +1,16 @@
+<script setup>
+import { useDark } from "@vueuse/core";
+import SearchBar from "../components/SearchBar.vue";
+import Header from "../components/Header.vue";
+
+const isDark = useDark();
+</script>
+
 <template>
-	<div>
-		<h1 class="text-center">News</h1>
+	<div class="col-12 d-flex align-items-center mt-5">
+		<SearchBar page="News" placeholder="Pesquise por notícias" />
+	</div>
+	<div class="col-12">
+		<Header title="NOTÍCIAS" />
 	</div>
 </template>

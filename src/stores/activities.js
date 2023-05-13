dbc2f553cdc0d4220a208b64af00fc77d95e4cf9
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-13 15:11:05 +0100

    Created activities store

---
+++ src/stores/activities.js
@@ -0,0 +1,16 @@
+import api from "@/services/api";
+import { defineStore } from "pinia";
+
+export const useActivitiesStore = defineStore("activities", () => {
+	const search = async (input) => {
+		try {
+			const response = await api.get(`/activities?search=${input}`);
+			return response.data;
+		} catch (err) {
+			console.log(err.response.data || "Error in search activities");
+			return { success: false, data: [] };
+		}
+	};
+
+	return { search };
+});

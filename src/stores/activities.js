d8aa3910966fbc9fb3d7533e3422cb45d4d322ba
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-15 21:09:52 +0100

    Added delete activity api call

--- src/stores/activities.js
+++ src/stores/activities.js
@@ -1,5 +1,6 @@
 import api from "@/services/api";
 import { defineStore } from "pinia";
+import { useUsersStore } from "./users";
 
 export const useActivitiesStore = defineStore("activities", () => {
 	/** @param {string} input @returns {Promise<{success: boolean, data: []}>} */
@@ -12,5 +13,16 @@ export const useActivitiesStore = defineStore("activities", () => {
 		}
 	};
 
-	return { search };
+	const deleteActivity = async (id) => {
+		const token = useUsersStore().token;
+
+		try {
+			const response = await api.delete(`/activities/${id}`).set("Authorization", `Bearer ${token}`);
+			return response.data;
+		} catch (err) {
+			return { success: false, message: "Ocorreu um erro ao apagar a atividade" };
+		}
+	};
+
+	return { search, deleteActivity };
 });

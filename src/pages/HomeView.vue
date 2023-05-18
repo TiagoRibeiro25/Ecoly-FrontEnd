3575714f50b361ed11fcb99fb54426398d39a236
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-18 23:41:03 +0100

    Added random quote

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -6,6 +6,10 @@ import RecentActivities from "../components/Home/RecentActivities.vue";
 import { useNewsStore } from "../stores/news";
 import { onBeforeMount, ref } from "vue";
 import { useActivitiesStore } from "../stores/activities";
+import { useDark } from "@vueuse/core";
+import axios from "axios";
+
+const isDark = useDark();
 
 // News Variables
 const recentNews = ref([]);
@@ -14,6 +18,10 @@ const fetchingNews = ref(true);
 // Activities Variables
 const recentActivities = ref([]);
 const fetchingActivities = ref(true);
+
+// Quotes Variables
+const quote = ref({});
+const fetchingQuote = ref(true);
 
 /** @returns {Promise<void>} */
 const fetchNews = async () => {
@@ -33,8 +41,26 @@ const fetchActivities = async () => {
 	fetchingActivities.value = false;
 };
 
+/** @returns {Promise<void>} */
+const fetchRandomQuote = async () => {
+	const url = import.meta.env.VITE_QUOTES_API_URL;
+	try {
+		const response = await axios.get(url);
+		quote.value = response.data;
+	} catch (err) {
+		console.log("There was an error fetching the quote. Using default quote instead.\n Error: ", err);
+		quote.value = {
+			author: "Yan Arthus Bertrand",
+			content:
+				"A ecologia deve estar inscrita no ADN de tudo o que fazemos diariamente e ainda não entrou nos costumes.",
+		};
+	} finally {
+		fetchingQuote.value = false;
+	}
+};
+
 onBeforeMount(() => {
-	Promise.all([fetchNews(), fetchActivities()]);
+	Promise.all([fetchNews(), fetchActivities(), fetchRandomQuote()]);
 });
 </script>
 
@@ -53,6 +79,47 @@ onBeforeMount(() => {
 			<div class="col-12 px-0 mt-5">
 				<RecentActivities :activities="recentActivities" :fetching="fetchingActivities" />
 			</div>
+			<div class="col-12 px-0 mt-5">
+				<div class="d-flex flex-column align-items-center justify-content-center mb-5 w-75 mx-auto pt-5">
+					<div
+						v-if="quote.author && quote.content"
+						class="text-center"
+						:class="isDark ? 'dark-text' : 'light-text'"
+					>
+						<p class="content mb-0">{{ quote.content }}</p>
+						<span class="author mt-0">{{ quote.author }}</span>
+					</div>
+					<div v-else>
+						<b-spinner variant="success" label="Carregando..."></b-spinner>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
+
+<style lang="scss" scoped>
+$primary-color: #f8f9fa;
+$secondary-color: #333333;
+
+.content {
+	font-family: "Panton", sans-serif;
+	font-size: 1.7rem;
+	font-weight: 700;
+}
+
+.author {
+	font-family: "Panton", sans-serif;
+	font-size: 1.1rem;
+	font-weight: 500;
+	opacity: 0.7;
+}
+
+.light-text {
+	color: $secondary-color;
+}
+
+.dark-text {
+	color: $primary-color;
+}
+</style>

ea25eebe047f570f0861abcdd5ee2375bf23e681
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-18 21:51:34 +0100

    Create Arrow.vue

---
+++ src/components/Arrow.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { useDark } from "@vueuse/core";
+
+const isDark = useDark();
+const props = defineProps({
+	options: { type: Object, required: true },
+});
+const emit = defineEmits(["toggleTheme"]);
+
+function handleClick() {
+	if (props.options.isDisabled) return;
+	emit("click");
+}
+</script>
+<template>
+	<div v-if="options.direction === 'right'" @click="handleClick" class="mr-3">
+		<img v-if="!isDark" src="@/assets/icons/rightLightThemeArrow.svg" alt="Seta para a direita" />
+		<img v-else src="@/assets/icons/rightDarkThemeArrow.svg" alt="Seta para a direita" />
+	</div>
+	<div v-else @click="handleClick" class="ml-3">
+		<img v-if="!isDark" src="@/assets/icons/leftLightThemeArrow.svg" alt="Seta para a direita" />
+		<img v-else src="@/assets/icons/leftDarkThemeArrow.svg" alt="Seta para a direita" />
+	</div>
+</template>
+
+<style lang="scss" scoped>
+div {
+	margin-top: 145px;
+	cursor: pointer;
+	transition: 0.3s;
+
+	&:hover {
+		scale: 1.05;
+	}
+
+	&[disabled] {
+		opacity: 0.5;
+		cursor: default;
+	}
+}
+</style>

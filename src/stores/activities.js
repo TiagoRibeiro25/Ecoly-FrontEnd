da2d4270f06c77a13f0bc4d8b248872ee1f30466
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-21 14:32:07 +0100

    Fixed delete activity api call

--- src/stores/activities.js
+++ src/stores/activities.js
@@ -23,10 +23,11 @@ export const useActivitiesStore = defineStore("activities", () => {
 	};
 
 	const deleteActivity = async (id) => {
-		const token = useUsersStore().token;
+		const usersStore = useUsersStore();
+		const headers = { Authorization: `Bearer ${usersStore.token}` };
 
 		try {
-			const response = await api.delete(`/activities/${id}`).set("Authorization", `Bearer ${token}`);
+			const response = await api.delete(`/activities/${id}`, { headers });
 			return response.data;
 		} catch (err) {
 			return { success: false, message: "Ocorreu um erro ao apagar a atividade" };

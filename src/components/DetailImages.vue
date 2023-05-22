afbc745165e7080e57eba62a9ec14112907140e2
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-22 19:06:01 +0100

    Update DetailImages.vue
    Clicking on the image zooms it

--- src/components/DetailImages.vue
+++ src/components/DetailImages.vue
@@ -1,37 +1,83 @@
 <script setup>
-import { ref } from "vue";
+import { ref, defineProps, onMounted, onUnmounted } from "vue";
 
 const props = defineProps({
 	images: { type: Array, required: true },
 });
 
 const dataImages = ref([]);
+const selectedImage = ref(null);
+const modalRef = ref(null);
 
 let i = 0;
 const internal = setInterval(() => {
 	if (i < props.images.length) {
 		dataImages.value.push(props.images[i]);
 		i++;
-	} else clearInterval(internal);
+	} else {
+		clearInterval(internal);
+	}
 }, 200);
+
+function openModal(index) {
+	selectedImage.value = dataImages.value[index];
+	modalRef.value.classList.add("show");
+}
+
+function closeModal(event) {
+	if (event.target === modalRef.value) {
+		modalRef.value.classList.remove("show");
+	}
+}
+
+// Close the modal when the Escape key is pressed
+window.addEventListener("keydown", (event) => {
+	if (event.key === "Escape") {
+		modalRef.value.classList.remove("show");
+	}
+});
+
+// Close the modal when the user clicks outside of it
+onMounted(() => {
+	window.addEventListener("click", closeModal);
+});
+
+// Cleanup the event listener when the component is unmounted
+onUnmounted(() => {
+	window.removeEventListener("click", closeModal);
+});
 </script>
 
 <template>
-	<div
-		v-if="dataImages.length !== 0"
-		class="col-12 h-100 d-xl-block d-flex justify-content-center align-items-center"
-	>
-		<img
-			v-for="(image, index) in dataImages"
-			:key="index"
-			v-lazy="image"
-			class="newDetailsImg img-fluid my-3 mx-3 rounded-lg shadow"
-			alt="Imagem do Item"
-		/>
-	</div>
-	<div v-else class="col-12" style="height: 190px">
-		<div class="col-12 d-flex justify-content-center align-items-center">
-			<b-spinner variant="success" label="Carregando..."></b-spinner>
+	<div>
+		<div
+			v-if="dataImages.length !== 0"
+			class="col-12 h-100 d-xl-block d-flex justify-content-center align-items-center"
+		>
+			<img
+				v-for="(image, index) in dataImages"
+				:key="index"
+				:src="image"
+				@click="openModal(index)"
+				class="newDetailsImg img-fluid my-3 mx-3 rounded-lg shadow"
+				alt="Imagem do Item"
+			/>
+		</div>
+		<div v-else class="col-12" style="height: 190px">
+			<div class="col-12 d-flex justify-content-center align-items-center">
+				<b-spinner variant="success" label="Carregando..."></b-spinner>
+			</div>
+		</div>
+
+		<!-- Modal -->
+		<div class="modal" tabindex="-1" ref="modalRef" @click="closeModal">
+			<div class="modal-dialog modal-dialog-centered modal-xl">
+				<div class="modal-content">
+					<div class="modal-body d-flex justify-content-center align-items-center">
+						<img :src="selectedImage" class="img-fluid" alt="Imagem ampliada" />
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -41,6 +87,7 @@ const internal = setInterval(() => {
 	width: 268px;
 	height: 170px;
 	animation: slideIn 1s ease-in-out;
+	cursor: pointer;
 }
 
 @keyframes slideIn {
@@ -52,5 +99,14 @@ const internal = setInterval(() => {
 		transform: translateX(0) scale(1);
 		opacity: 1;
 	}
+}
+
+.modal {
+	display: none;
+	background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal.show {
+	display: block;
 }
 </style>

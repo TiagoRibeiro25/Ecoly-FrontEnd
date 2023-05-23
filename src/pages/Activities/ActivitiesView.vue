cf7bce27424573313a8f66965febb19a1980fd79
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-23 19:09:49 +0100

    started activities view

--- src/pages/Activities/ActivitiesView.vue
+++ src/pages/Activities/ActivitiesView.vue
@@ -1,9 +1,74 @@
 <script setup>
-import { useDark } from "@vueuse/core";
 import SearchBar from "../../components/SearchBar.vue";
 import Header from "../../components/Header.vue";
+import ChangeViewButton from "../../components/ChangeViewButton.vue";
+import { useDark } from "@vueuse/core";
+import { ref, watch, watchEffect } from "vue";
+import { useSchoolsStore } from "../../stores/schools";
+import { useActivitiesStore } from "../../stores/activities";
+import ActivityCard from "./ActivityCard.vue";
 
 const isDark = useDark();
+const fetching = ref(false);
+const activities = ref([]);
+const schools = ref([{ value: "all", text: "Todas as escolas" }]);
+const schoolSelected = ref("all");
+const isUserVerified = ref(false);
+
+// delete modal variables
+const showDeleteModal = ref(false);
+const idToDelete = ref(null);
+const fetchAgain = ref(false);
+
+const showModal = (id) => {
+	idToDelete.value = id;
+	showDeleteModal.value = true;
+};
+
+watch(schoolSelected, () => {
+	fetchAgain.value = !fetchAgain.value;
+});
+
+watchEffect(async () => {
+	if (!fetchAgain.value && activities.value.length !== 0) return;
+
+	fetchAgain.value = false;
+	fetching.value = true;
+	activities.value = [];
+	schools.value = [{ value: "all", text: "Todas as escolas" }];
+	isUserVerified.value = false;
+
+	const schoolsStore = useSchoolsStore();
+	const schoolsResponse = await schoolsStore.getSchools();
+
+	if (schoolsResponse.success) {
+		schools.value = schools.value.concat(
+			schoolsResponse.data.map((school) => {
+				return { value: school.id, text: school.name };
+			})
+		);
+	}
+
+	const activitiesStore = useActivitiesStore();
+	let activitiesResponse = null;
+
+	// Get all activities
+	if (schoolSelected.value === "all") {
+		activitiesResponse = await activitiesStore.getAllUnfinishedActivities();
+	}
+	// Get activities from a specific school
+	else {
+		const schoolName = schools.value.find((school) => school.value === schoolSelected.value).text;
+		activitiesResponse = await activitiesStore.getUnfinishedActivitiesFromSchool(schoolName);
+	}
+
+	if (activitiesResponse.success) {
+		activities.value = activitiesResponse.data;
+		isUserVerified.value = activitiesResponse.isUserVerified;
+	}
+
+	fetching.value = false;
+});
 </script>
 
 <template>
@@ -13,4 +78,112 @@ const isDark = useDark();
 	<div class="col-12">
 		<Header title="ATIVIDADES" />
 	</div>
+	<div class="col-12 mb-4 px-0">
+		<div class="activities-container mx-auto px-5 d-flex flex-row">
+			<div v-if="isUserVerified" class="col-6 px-0 d-flex align-items-end">
+				<ChangeViewButton text="Adicionar atividade" to="ActivityCreate" iconImg="../assets/icons/add.svg" />
+			</div>
+			<div class="d-flex justify-content-end px-0" :class="isUserVerified ? 'col-6' : 'col-12'">
+				<b-form-select
+					class="mt-3 school-select"
+					:class="isDark ? 'school-dark-mode' : 'school-light-mode'"
+					v-model="schoolSelected"
+					:options="schools"
+				></b-form-select>
+			</div>
+		</div>
+	</div>
+	<div class="col-12 mt-5"></div>
+
+	<!-- Activities -->
+	<div class="col-12">
+		<div class="activities-container mx-auto px-5">
+			<div
+				v-if="fetching || activities.length === 0"
+				class="d-flex flex-column align-items-center justify-content-center"
+				style="height: 45vh"
+			>
+				<b-spinner v-if="fetching" variant="success" label="Carregando..."></b-spinner>
+				<span v-else class="error-msg" :class="isDark ? 'error-msg-dark' : 'error-msg-light'">
+					Nenhuma atividade encontrada
+				</span>
+			</div>
+
+			<div v-else>
+				<ActivityCard
+					v-for="(activity, index) in activities"
+					:key="activity.id"
+					:index="index"
+					:id="activity.id"
+					:title="activity.title"
+					:description="activity.description"
+					:initialDate="activity.initial_date"
+					:finalDate="activity.final_date"
+					:theme="activity.theme"
+					:image="activity.image"
+					:canUserEdit="activity.canUserEdit"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
+
+<style lang="scss" scoped>
+$primary-color: #343e3d;
+$secondary-color: #aedcc0;
+$tertiary-color: #ffffff;
+$quaternary-color: #3fc380;
+$quinary-color: #18516f;
+
+.activities-container {
+	max-width: 1400px;
+}
+
+.error-msg {
+	font-family: "Panton", sans-serif;
+	font-weight: 700;
+	font-size: 1.7rem;
+}
+
+.error-msg-dark {
+	color: $secondary-color;
+}
+
+.error-msg-light {
+	color: $primary-color;
+}
+
+.school-select {
+	font-family: "Panton", sans-serif;
+	font-size: 1.1rem;
+	font-weight: 400;
+
+	background-color: transparent;
+	outline: transparent;
+	border: 2px solid $quaternary-color;
+	border-radius: 0.6rem;
+	width: auto;
+}
+
+.school-dark-mode {
+	color: $secondary-color;
+
+	&:focus {
+		border: 2px solid $tertiary-color;
+		outline: transparent;
+		box-shadow: none;
+		color: $tertiary-color;
+		background-color: $primary-color;
+	}
+}
+
+.school-light-mode {
+	color: $primary-color;
+
+	&:focus {
+		border: 2px solid $quinary-color;
+		outline: transparent;
+		box-shadow: none;
+	}
+}
+</style>
---
+++ src/pages/Activities/ActivityCard.vue
@@ -0,0 +1,160 @@
+<script setup>
+import CardImage from "../../components/CardImage.vue";
+import { useDark } from "@vueuse/core";
+import { ref, onMounted } from "vue";
+
+const props = defineProps({
+	index: { type: Number, required: true },
+	id: { type: Number, required: true },
+	title: { type: String, required: true },
+	description: { type: String, required: true },
+	initialDate: { type: String, required: true },
+	finalDate: { type: String, required: true },
+	theme: { type: String, required: true },
+	image: { type: String, required: true },
+	canUserEdit: { type: Boolean, required: false, default: false },
+});
+
+const emit = defineEmits(["delete", "finish"]);
+
+const isDark = useDark();
+const loaded = ref(false);
+
+onMounted(() => {
+	setTimeout(() => {
+		loaded.value = true;
+	}, (props.index + 1) * 100);
+});
+</script>
+
+<template>
+	<div
+		v-if="loaded"
+		class="activity-card mb-5 shadow d-flex flex-row"
+		:class="isDark ? 'activity-card-bg-dark' : 'activity-card-bg-light'"
+	>
+		<div class="col-4 px-0 d-lg-block d-none">
+			<CardImage :image="props.image" type="activity" :id="props.id" />
+		</div>
+		<div class="col-lg-8 d-flex flex-column px-2 py-3" style="overflow: hidden">
+			<div class="row">
+				<div
+					class="title-container text-lg-left text-center"
+					:class="props.canUserEdit ? 'col-10' : 'col-12'"
+				>
+					<h2>
+						<router-link
+							:to="{ name: 'ActivityDetail', params: { id: props.id } }"
+							class="activity-title"
+							:class="isDark ? 'title-dark' : 'title-light'"
+						>
+							{{ props.title }}
+						</router-link>
+					</h2>
+				</div>
+				<div v-if="props.canUserEdit" class="col-2 text-center pt-2">
+					<b-button
+						size="sm"
+						class="rounded-circle bg-transparent border-0"
+						@click="emit('delete', props.id)"
+					>
+						<img
+							v-lazy="{
+								src: isDark ? '../assets/icons/remove-dark.svg' : '../assets/icons/remove-light.svg',
+							}"
+							alt="Remover atividade"
+							width="25"
+							height="25"
+						/>
+					</b-button>
+				</div>
+			</div>
+			<div class="row mt-3">
+				<div class="col-12">
+					<p
+						class="activity-description text-justify pr-1"
+						:class="isDark ? 'activity-description-dark' : 'activity-description-light'"
+					>
+						{{ props.description }}
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+$primary-color: #343e3d;
+$secondary-color: #e4f0e8;
+$tertiary-color: #ffffff;
+$quaternary-color: #aedcc0;
+
+.activity-card {
+	border-radius: 20px;
+	height: 300px;
+	animation: slideIn 0.7s ease-in-out;
+}
+
+.activity-title {
+	font-family: "Alkes", sans-serif;
+	font-size: 1.35rem;
+	font-weight: 700;
+
+	&:hover {
+		text-decoration: underline;
+	}
+}
+
+.title-container {
+	height: 70px;
+	overflow: hidden;
+}
+
+.activity-description,
+.activity-date {
+	font-family: "Panton", sans-serif;
+	font-size: 1rem;
+	font-weight: 400;
+	height: 80px;
+	overflow: hidden;
+}
+
+.activity-description {
+	&-dark {
+		color: $tertiary-color;
+	}
+
+	&-light {
+		color: $primary-color;
+	}
+}
+
+.activity-date {
+	font-size: 0.9rem;
+}
+
+.title-dark {
+	color: $quaternary-color;
+}
+
+.title-light {
+	color: $primary-color;
+}
+
+.activity-card-bg-dark {
+	background-color: $primary-color;
+}
+
+.activity-card-bg-light {
+	background-color: $secondary-color;
+}
+
+@keyframes slideIn {
+	0% {
+		transform: translateX(-100%);
+	}
+	100% {
+		transform: translateX(0);
+	}
+}
+</style>

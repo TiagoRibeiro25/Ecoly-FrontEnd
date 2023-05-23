98e27394297cc5593d92a696f232422a50132fa4
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-23 10:44:51 +0100

    Added api calls to add and edit a school

--- src/stores/schools.js
+++ src/stores/schools.js
@@ -1,5 +1,6 @@
 import api from "@/services/api";
 import { defineStore } from "pinia";
+import { useUsersStore } from "./users";
 
 export const useSchoolsStore = defineStore("schools", () => {
 	const getSchools = async () => {
@@ -11,5 +12,29 @@ export const useSchoolsStore = defineStore("schools", () => {
 		}
 	};
 
-	return { getSchools };
+	const addSchool = async (school) => {
+		const usersStore = useUsersStore();
+		const headers = { Authorization: `Bearer ${usersStore.token}` };
+
+		try {
+			const response = await api.post("/schools", { name: school }, { headers });
+			return response.data;
+		} catch (err) {
+			return { success: false, message: "Erro ao adicionar escola" };
+		}
+	};
+
+	const editSchool = async (schoolId, newName) => {
+		const usersStore = useUsersStore();
+		const headers = { Authorization: `Bearer ${usersStore.token}` };
+
+		try {
+			const response = await api.put(`/schools/${schoolId}`, { name: newName }, { headers });
+			return response.data;
+		} catch (err) {
+			return { success: false, message: "Erro ao editar escola" };
+		}
+	};
+
+	return { getSchools, addSchool, editSchool };
 });

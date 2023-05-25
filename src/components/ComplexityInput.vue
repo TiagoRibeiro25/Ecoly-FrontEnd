25c29e75422820a3a78af7e97f611ec1c4bbe49f
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-25 10:51:06 +0100

    Create ComplexityInput.vue

---
+++ src/components/ComplexityInput.vue
@@ -0,0 +1,67 @@
+<script setup>
+
+import { ref, watch } from "vue";
+
+const emits = defineEmits(["update:complexity"]);
+const props = defineProps({
+	complexity: { type: Number, required: true },
+	isDark: { type: Boolean, required: false, default: false }
+});
+
+const complexity = ref(props.complexity);
+
+watch(complexity, () => {
+	emits("update:complexity", complexity.value);
+});
+
+</script>
+
+<template>
+	<div>
+		<b class="mr-1 info-label" :class="props.isDark ? 'info-dark-theme' : 'info-light-theme'">
+			Complexidade
+		</b>
+		<b-form-rating
+			class="complexity-info border-0"
+			:class="props.isDark ? 'complexity-dark-theme' : 'complexity-light-theme'"
+			inline
+			v-model="complexity"
+		></b-form-rating>
+	</div>
+</template>
+
+<style scoped lang="scss">
+$primary-color: #343e3d;
+$secondary-color: #aedcc0;
+$tertiary-color: #3fc380;
+$fourth-color: #ffffff;
+
+.info-label {
+	font-family: "Panton", sans-serif;
+	font-size: 1.5rem;
+	font-weight: 600;
+}
+
+.complexity-info {
+	font-size: 1.3rem;
+	transform: translateY(2px);
+	background-color: transparent;
+}
+
+.info-light-theme,
+.complexity-light-theme {
+	color: $primary-color;
+}
+
+.info-dark-theme {
+	color: $tertiary-color;
+}
+
+.complexity-dark-theme {
+	color: $secondary-color;
+
+	&:hover {
+		color: $fourth-color;
+	}
+}
+</style>

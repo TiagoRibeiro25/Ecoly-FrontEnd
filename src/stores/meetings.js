b9e44ef855e33cbf1423f69c1fe0abbb4c877f63
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-27 23:06:25 +0100

    feat: :hammer: Add api call to get meetings (past | future)

--- src/stores/meetings.js
+++ src/stores/meetings.js
@@ -3,6 +3,10 @@ import { defineStore } from "pinia";
 import { useUsersStore } from "./users";
 
 export const useMeetingsStore = defineStore("meetings", () => {
+	/**
+	 * @param {{date: string, description: string, room: string}} meeting
+	 * @returns {Promise<{success: boolean, message: string}>}
+	 */
 	const createMeeting = async (meeting) => {
 		const usersStore = useUsersStore();
 		const headers = { Authorization: `Bearer ${usersStore.token}` };
@@ -15,6 +19,21 @@ export const useMeetingsStore = defineStore("meetings", () => {
 		}
 	};
 
+	/**
+	 * @param {"past" | "future"} type
+	 * @returns {Promise<{success: boolean, error?: string, data?: {hasAta?: boolean, id: number, creator: {id: number, name: string}, date: string, room: string description: string}[]}>}
+	 */
+	const getMeetings = async (type) => {
+		const usersStore = useUsersStore();
+		const headers = { Authorization: `Bearer ${usersStore.token}` };
 
-	return { createMeeting };
+		try {
+			const response = await api.get(`/meetings/?filter=${type}`, { headers });
+			return response.data;
+		} catch (err) {
+			return { success: false, error: "Erro ao obter reuniões" };
+		}
+	};
+
+	return { createMeeting, getMeetings };
 });

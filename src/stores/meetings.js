f01fd46f5fea0099b4f26728958a9a0aa48d3f3d
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-28 17:50:04 +0100

    feat: :sparkles: Added api call to get an ata from a specific meeting

--- src/stores/meetings.js
+++ src/stores/meetings.js
@@ -21,7 +21,7 @@ export const useMeetingsStore = defineStore("meetings", () => {
 
 	/**
 	 * @param {"past" | "future"} type
-	 * @returns {Promise<{success: boolean, error?: string, data?: {hasAta?: boolean, id: number, creator: {id: number, name: string}, date: string, room: string description: string}[]}>}
+	 * @returns {Promise<{success: boolean, message?: string, data?: {hasAta?: boolean, id: number, creator: {id: number, name: string}, date: string, room: string description: string}[]}>}
 	 */
 	const getMeetings = async (type) => {
 		const usersStore = useUsersStore();
@@ -31,9 +31,25 @@ export const useMeetingsStore = defineStore("meetings", () => {
 			const response = await api.get(`/meetings/?filter=${type}`, { headers });
 			return response.data;
 		} catch (err) {
-			return { success: false, error: "Erro ao obter reuniões" };
+			return { success: false, message: "Erro ao obter reuniões" };
 		}
 	};
 
-	return { createMeeting, getMeetings };
+	/**
+	 * @param {number} id
+	 * @returns {Promise<{success: boolean, message?: string, data?: {ata: string, date: string, images: string[]}}>}
+	 */
+	const getMeetingAta = async (id) => {
+		const usersStore = useUsersStore();
+		const headers = { Authorization: `Bearer ${usersStore.token}` };
+
+		try {
+			const response = await api.get(`/meetings/${id}?fields=ata`, { headers });
+			return response.data;
+		} catch (err) {
+			return { success: false, message: "Erro ao obter ata da reunião" };
+		}
+	};
+
+	return { createMeeting, getMeetings, getMeetingAta };
 });

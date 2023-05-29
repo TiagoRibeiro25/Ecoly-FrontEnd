8256c35ee6a07d8b9a82d7420c6e306e0f5e3a22
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-29 18:58:40 +0100

    refactor: :lock: Prevent from seeing dashboard view

--- src/pages/Dashboard/DashboardView.vue
+++ src/pages/Dashboard/DashboardView.vue
@@ -1,12 +1,20 @@
 <script setup>
-import { useDark } from "@vueuse/core";
+import { onBeforeMount } from "vue";
 import Header from "../../components/Header.vue";
+import { useRouter } from "vue-router";
 
-const isDark = useDark();
+onBeforeMount(async () => {
+	const router = useRouter();
+	await router.push({ name: "NotFound" });
+});
 </script>
 
 <template>
 	<div class="col-12 mt-5">
 		<Header title="DASHBOARD" />
+	</div>
+
+	<div class="d-flex flex-column align-items-center justify-content-center" style="height: 70vh">
+		<b-spinner variant="success" label="Carregando..."></b-spinner>
 	</div>
 </template>

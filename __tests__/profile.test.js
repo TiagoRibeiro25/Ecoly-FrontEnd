713c2b2c24b0d3e0a203b257f81789496b265449
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-06-02 12:51:33 +0100

    test: :white_check_mark: Updated edit profile test

--- __tests__/profile.test.js
+++ __tests__/profile.test.js
@@ -3,6 +3,17 @@ const firefox = require("selenium-webdriver/firefox");
 const colors = require("colors");
 const login = require("./login.js");
 
+async function clearForm(email, password, confirmPassword, internalNumber, course, year) {
+	await Promise.all([
+		email.clear(),
+		password.clear(),
+		confirmPassword.clear(),
+		internalNumber.clear(),
+		course.clear(),
+		year.clear(),
+	]);
+}
+
 module.exports = async (url) => {
 	try {
 		const driver = await new Builder().forBrowser("firefox").withCapabilities(new firefox.Options()).build();
@@ -11,12 +22,7 @@ module.exports = async (url) => {
 		await login({ driver, email: "user@esmad.ipp.pt", password: "Esmad_2223" });
 
 		//* Edit profile
-		let email;
-		let password;
-		let confirmPassword;
-		let internalNumber;
-		let course;
-		let year;
+		let email, password, confirmPassword, internalNumber, course, year;
 
 		await driver.wait(until.elementLocated(By.className("edit-profile-btn"))).click();
 
@@ -32,20 +38,17 @@ module.exports = async (url) => {
 		year = await driver.wait(until.elementLocated(By.css("input[placeholder='Ano do Curso (opcional)']")));
 
 		// clear the fields
-		await email.clear();
-		await password.clear();
-		await confirmPassword.clear();
-		await internalNumber.clear();
-		await course.clear();
-		await year.clear();
+		await clearForm(email, password, confirmPassword, internalNumber, course, year);
 
 		// New data
-		email.sendKeys("user.updated@esmad.ipp.pt");
-		password.sendKeys("Esmad_2223");
-		confirmPassword.sendKeys("Esmad_2223");
-		internalNumber.sendKeys("123456789");
-		course.sendKeys("TSIW - Updated");
-		year.sendKeys("3");
+		await Promise.all([
+			email.sendKeys("user.updated@esmad.ipp.pt"),
+			password.sendKeys("Esmad_2223"),
+			confirmPassword.sendKeys("Esmad_2223"),
+			internalNumber.sendKeys("123456789"),
+			course.sendKeys("TSIW - Updated"),
+			year.sendKeys("3"),
+		]);
 
 		// click on the save button
 		await driver.wait(until.elementLocated(By.css("button[type='submit']"))).click();
@@ -70,20 +73,17 @@ module.exports = async (url) => {
 		year = await driver.wait(until.elementLocated(By.css("input[placeholder='Ano do Curso (opcional)']")));
 
 		// clear the fields
-		await email.clear();
-		await password.clear();
-		await confirmPassword.clear();
-		await internalNumber.clear();
-		await course.clear();
-		await year.clear();
+		await clearForm(email, password, confirmPassword, internalNumber, course, year);
 
 		// New data
-		email.sendKeys("user@esmad.ipp.pt");
-		password.sendKeys("Esmad_2223");
-		confirmPassword.sendKeys("Esmad_2223");
-		internalNumber.sendKeys("40210462");
-		course.sendKeys("TSIW");
-		year.sendKeys("2");
+		await Promise.all([
+			email.sendKeys("user@esmad.ipp.pt"),
+			password.sendKeys("Esmad_2223"),
+			confirmPassword.sendKeys("Esmad_2223"),
+			internalNumber.sendKeys("40210462"),
+			course.sendKeys("TSIW"),
+			year.sendKeys("2"),
+		]);
 
 		await driver.wait(until.elementLocated(By.css("button[type='submit']"))).click();
 

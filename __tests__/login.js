4a44576ec84d8ec4410a42e1b5fe8f129fe81951
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-06-04 21:30:13 +0100

    test: :white_check_mark: Fixed login function used in the tests

--- __tests__/login.js
+++ __tests__/login.js
@@ -1,14 +1,31 @@
 const { By, until } = require("selenium-webdriver");
+const colors = require("colors");
 
 module.exports = async ({ driver, email, password }) => {
 	await driver.wait(until.elementLocated(By.css("a[href='/authenticate']"))).click();
 
 	const loginEmail = await driver.wait(until.elementLocated(By.css("input[type='email']")));
+
+	// clear the email field
+	await loginEmail.clear();
+
+	// re-locate the password field
 	const loginPassword = await driver.wait(until.elementLocated(By.css("input[type='password']")));
 
-	await loginEmail.sendKeys(email);
-	await loginPassword.sendKeys(password);
+	// clear the password field
+	await loginPassword.clear();
+
+	// re-locate the email field again
+	const reLocatedEmail = await driver.wait(until.elementLocated(By.css("input[type='email']")));
+
+	// re-locate the password field again
+	const reLocatedPassword = await driver.wait(until.elementLocated(By.css("input[type='password']")));
+
+	// enter email and password
+	await Promise.all([reLocatedEmail.sendKeys(email), reLocatedPassword.sendKeys(password)]);
 
 	// click on the login button
 	await driver.wait(until.elementLocated(By.css("button[type='submit']"))).click();
+
+	await driver.sleep(800);
 };

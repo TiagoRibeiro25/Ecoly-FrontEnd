f41f385ea3ca0d4f54584f7c3c33ae095a9a1e84
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-06-15 21:11:32 +0100

    feat: :sparkles: Added auto generate token between requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,24 @@
 import axios from "axios";
+import { useUsersStore } from "../stores/users";
 
 const api = axios.create({
 	baseURL: import.meta.env.VITE_API_ROUTE,
 	headers: { "Content-Type": "application/json" },
 });
+
+// Add a response interceptor - update token if it changes
+api.interceptors.response.use(
+	(response) => {
+		const newToken = response.headers["x-access-token"] || response.headers["authorization"];
+		if (newToken) {
+			useUsersStore().token = newToken.replace("Bearer ", "");
+		}
+
+		return response;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
 
 export default api;
